d35ae33d99e4c48c408293cb5cff7c364adb303e
Taylor August Giles <augustgiles8@gmail.com>
2018-03-03 04:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,5 +30,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 function theFunk() {
-  
+  var theFunk = funkyFunction()
+  return theFunk
 }
+theFunk()
