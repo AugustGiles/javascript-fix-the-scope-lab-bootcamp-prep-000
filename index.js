679e10e89d4cbc628d039456504919f58dfd7a5e
Taylor August Giles <augustgiles8@gmail.com>
2018-03-03 03:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var animal = 'dog'
 
 function myAnimal() {
+  var animal = "cat"
   return animal
 }
 
@@ -13,10 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
